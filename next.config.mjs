2504c11e4fb38bef8e73de1ac1d534980d87fbd8
Heikki Vuorinen <hjvuor@utu.fi>
2024-11-05 14:41:01 +0200

    modified config to keep pages in buffer

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
       },
     ],
   },
+  onDemandEntries: {
+    // how long pages are kept in the buffer (in ms)
+    maxInactiveAge: 60 * 1000,
+    // how many pages
+    pagesBufferLength: 10
+  }
 }
 
 export default nextConfig;
